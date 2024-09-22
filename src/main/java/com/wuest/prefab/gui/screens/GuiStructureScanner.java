b9d613a0c7ff51aa15e7f91a979644d9db768d66
Brian Wuest <brian.wuest@outlook.com>
2024-09-22 16:33:01 -0500

    Fixing issue with structure scanners

--- src/main/java/com/wuest/prefab/gui/screens/GuiStructureScanner.java
+++ src/main/java/com/wuest/prefab/gui/screens/GuiStructureScanner.java
@@ -150,8 +150,9 @@ public class GuiStructureScanner extends GuiBase {
             // This generally happens when leaving and re-joining the world.
             if (existingConfig != null) {
                 ClientModRegistry.structureScanners.remove(existingConfig);
-                ClientModRegistry.structureScanners.add(this.config);
             }
+
+            ClientModRegistry.structureScanners.add(this.config);
 
             this.closeScreen();
         } else {
