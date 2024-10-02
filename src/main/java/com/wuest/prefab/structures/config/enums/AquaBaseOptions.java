f89658cbc9a6a447dbbc5965a05fcb2fcb9c785e
Brian Wuest <brian.wuest@outlook.com>
2024-10-02 18:10:24 -0500

    1.20.6 - Fabric - Fixing zip file names

--- src/main/java/com/wuest/prefab/structures/config/enums/AquaBaseOptions.java
+++ src/main/java/com/wuest/prefab/structures/config/enums/AquaBaseOptions.java
@@ -3,7 +3,7 @@ package com.wuest.prefab.structures.config.enums;
 public class AquaBaseOptions extends BaseOption {
     public static AquaBaseOptions Default = new AquaBaseOptions(
             "item.prefab.aqua_base",
-            "assets/prefab/structures/aqua_base.zip",
+            "assets/prefab/structures/aqua_base.gz",
             "textures/gui/aqua_base.png",
             false,
             false);
